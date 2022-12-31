dbf856b1a322da20828abadca6140526b00a97d7
JoaquinAbadia1 <106445514+JoaquinAbadia1@users.noreply.github.com>
2022-12-31 13:21:17 -0300

    Update

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 function login() {
-    const usuario = document.getElementById('usuarioPrincipal').value;
+    const usuarioInput = document.getElementById('usuarioPrincipal').value;
     const password = document.getElementById('passPrincipal').value;
-    if(usuario === 'joaquin' && password === 'abadia' ){
-        localStorage.setItem("usuario", usuario);
+fetch('./usuario.json').then((response) => response.json()).then((usuario) => {
+if(usuarioInput === usuario.usuario && password === usuario.contraseña ){
+        localStorage.setItem("usuario", JSON.stringify(usuario));
         window.location.href ='inicio/inicio.html';
     }else {
         console.log('Incorrecto')
     }
 
 }
+})
